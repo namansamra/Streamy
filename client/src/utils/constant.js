7711bb9492c657a90b9d9b1ddf5eeb24487fb1dd
naman samra <namansamra1712@gmail.com>
2021-12-26 15:51:22 +0530

    build

--- client/src/utils/constant.js
+++
@@ -1,3 +0,0 @@
-export const mediaServer = "http://3.7.71.1:3000";
-
-export const backendServer = "http://localhost:8002";
--- client/src/utils/jwt.js
+++
@@ -1,7 +0,0 @@
-export function jwtDecode(t) {
-    let token = {};
-    token.raw = t;
-    token.header = JSON.parse(window.atob(t.split('.')[0]));
-    token.payload = JSON.parse(window.atob(t.split('.')[1]));
-    return (token)
-  }
